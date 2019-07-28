2605867a651f627b1aca26e192cb685f00008c40
Panot Wongkhot <panotza_ja@hotmail.com>
2019-07-28 18:50:10 +0700

    init

--- controller/main.go
+++ controller/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/EdlinOrg/prominentcolor"
@@ -17,6 +18,10 @@ const (
 	bottomLEDCount = 20
 	maxLED         = leftLEDCount + topLEDCount + rightLEDCount + bottomLEDCount
 )
+
+type color struct {
+	r, g, b uint8
+}
 
 func main() {
 	options := serial.OpenOptions{
@@ -38,50 +43,134 @@ func main() {
 		w:      port,
 		buf:    make([]byte, maxLED*3),
 	}
+
+	sig := make(chan struct{}, 4)
+	leftRet := make(chan []color)
+	topRet := make(chan []color)
+	rightRet := make(chan []color)
+	bottomRet := make(chan []color)
+	var wg sync.WaitGroup
+
+	go leftWorker(&wg, sig, leftRet)
+	go topWorker(&wg, sig, topRet)
+	go rightWorker(&wg, sig, rightRet)
+	go bottomWorker(&wg, sig, bottomRet)
+
+	writeColor := func(cs []color) {
+		for _, c := range cs {
+			led.WriteColor(c.r, c.b, c.b)
+		}
+	}
+
+	for {
+		sig <- struct{}{}
+		sig <- struct{}{}
+		sig <- struct{}{}
+		sig <- struct{}{}
+
+		wg.Wait()
+		writeColor(<-leftRet)
+		writeColor(<-topRet)
+		writeColor(<-rightRet)
+		writeColor(<-bottomRet)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func leftWorker(wg *sync.WaitGroup, sig chan struct{}, ret chan []color) {
 	bounds := screenshot.GetDisplayBounds(0)
 	c := capture{bounds.Dx(), bounds.Dy()}
 
 	for {
-		imgL := c.Left(100, 80)
-		processVertical(&led, imgL, leftLEDCount, true)
-		imgT := c.Top(100, 80)
-		processHorizontal(&led, imgT, topLEDCount, false)
-		imgR := c.Right(100, 80)
-		processVertical(&led, imgR, rightLEDCount, false)
-		imgB := c.Bottom(100, 80)
-		processHorizontal(&led, imgB, bottomLEDCount, true)
-		time.Sleep(time.Second)
+		select {
+		case <-sig:
+			wg.Add(1)
+			img := c.Left(100, 80)
+			ret <- processVertical(img, leftLEDCount, true)
+			wg.Done()
+		}
 	}
 }
 
-func processVertical(lc *ledController, img *image.RGBA, count int, swap bool) {
+func topWorker(wg *sync.WaitGroup, sig chan struct{}, ret chan []color) {
+	bounds := screenshot.GetDisplayBounds(0)
+	c := capture{bounds.Dx(), bounds.Dy()}
+
+	for {
+		select {
+		case <-sig:
+			wg.Add(1)
+			img := c.Top(100, 80)
+			ret <- processHorizontal(img, topLEDCount, false)
+			wg.Done()
+		}
+	}
+}
+
+func rightWorker(wg *sync.WaitGroup, sig chan struct{}, ret chan []color) {
+	bounds := screenshot.GetDisplayBounds(0)
+	c := capture{bounds.Dx(), bounds.Dy()}
+
+	for {
+		select {
+		case <-sig:
+			wg.Add(1)
+			img := c.Right(100, 80)
+			ret <- processVertical(img, rightLEDCount, false)
+			wg.Done()
+		}
+	}
+}
+
+func bottomWorker(wg *sync.WaitGroup, sig chan struct{}, ret chan []color) {
+	bounds := screenshot.GetDisplayBounds(0)
+	c := capture{bounds.Dx(), bounds.Dy()}
+
+	for {
+		select {
+		case <-sig:
+			wg.Add(1)
+			img := c.Bottom(100, 80)
+			ret <- processHorizontal(img, bottomLEDCount, true)
+			wg.Done()
+		}
+	}
+}
+
+func processVertical(img *image.RGBA, count int, swap bool) []color {
 	width := img.Bounds().Dx()
 	height := img.Bounds().Dy()
 	div := height / count
 
+	cs := make([]color, count)
 	for i := 0; i < count; i++ {
 		n := i
 		if swap {
 			n = count - i - 1
 		}
 		img := img.SubImage(image.Rect(0, div*n, width, div*(n+1)))
-		lc.WriteColor(getProminentColor(img))
+		r, g, b := getProminentColor(img)
+		cs[i] = color{r, g, b}
 	}
+	return cs
 }
 
-func processHorizontal(lc *ledController, img *image.RGBA, count int, swap bool) {
+func processHorizontal(img *image.RGBA, count int, swap bool) []color {
 	width := img.Bounds().Dx()
 	height := img.Bounds().Dy()
 	div := width / count
 
+	cs := make([]color, count)
 	for i := 0; i < count; i++ {
 		n := i
 		if swap {
 			n = count - i - 1
 		}
 		img := img.SubImage(image.Rect(div*n, 0, div*(n+1), height))
-		lc.WriteColor(getProminentColor(img))
+		r, g, b := getProminentColor(img)
+		cs[i] = color{r, g, b}
 	}
+	return cs
 }
 
 var noBGMask []prominentcolor.ColorBackgroundMask
